1e2b01ae7fefadf871f1b176e0234da69bc1cc0a
César García <cesar.garciamcs@proton.me>
2025-08-12 14:25:31 +0000

    ejemplos de 01

---
+++ 01_print.py
@@ -0,0 +1,19 @@
+###
+# 01 - print()
+# El módulo print() es el módulo que nos permite imprimir en la consola
+# Sirve para mostrar informcación en consola y te va a acompañar toda tu vida
+###
+
+print("Hola, Python")
+
+print('Esto también funciona con comillas simples')
+
+print("Python", "es", "genial")
+
+print("Python", "es", "genial", sep="-")
+
+print("Esto se imprime", end = "\n")
+
+print("en una sola línea")
+
+print(42)
